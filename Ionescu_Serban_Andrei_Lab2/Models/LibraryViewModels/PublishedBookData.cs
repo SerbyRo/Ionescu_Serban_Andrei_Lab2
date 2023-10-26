e8ce545b8f8bac5446abb090f3d8b76d375cc872
SerbyRo <serbanandrei.ionescu@gmail.com>
2023-10-26 18:44:57 +0300

    First commit

--- Ionescu_Serban_Andrei_Lab2/Models/LibraryViewModels/PublishedBookData.cs
+++ Ionescu_Serban_Andrei_Lab2/Models/LibraryViewModels/PublishedBookData.cs
@@ -2,5 +2,8 @@
 {
     public class PublishedBookData
     {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public bool IsPublished { get; set; }
     }
 }
