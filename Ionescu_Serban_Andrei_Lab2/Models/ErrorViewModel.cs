d2e26f18560b147ab234d764a072bf4420eb9e98
SerbyRo <serbanandrei.ionescu@gmail.com>
2023-10-27 21:45:39 +0300

    Made Customer Controller

--- Ionescu_Serban_Andrei_Lab2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Ionescu_Serban_Andrei_Lab2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
