82739a6319ce0d7146c76b0cb3c5edbecda4a281
SerbyRo <serbanandrei.ionescu@gmail.com>
2024-01-12 17:53:15 +0200

    Commit all changes

---
+++ Ionescu_Serban_Andrei_Lab2/Controllers/PublishersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ionescu_Serban_Andrei_Lab2.Controllers
+{
+    [Authorize(Policy = "OnlySales")]
+    public class PublishersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Ionescu_Serban_Andrei_Lab2/Program.cs
+++ Ionescu_Serban_Andrei_Lab2/Program.cs
@@ -1,6 +1,8 @@
 using Ionescu_Serban_Andrei_Lab2.Data;
 using Ionescu_Serban_Andrei_Lab2.Hubs;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +10,36 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<LibraryContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+}).AddEntityFrameworkStores<IdentityContext>();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Lockout.DefaultLockoutTimeSpan= TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts= 3;
+    options.Lockout.AllowedForNewUsers= true;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 8;
+});
+
 builder.Services.AddSignalR();
+
+builder.Services.AddDbContext<IdentityContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthorization(opts =>
+{
+    opts.AddPolicy("OnlySales", policy =>
+    {
+        policy.RequireClaim("Department", "Sales");
+    });
+});
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
@@ -29,7 +60,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
@@ -37,6 +68,7 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapHub<ChatHub>("/Chat");
+app.MapRazorPages();
 
 app.Run();
 //works
